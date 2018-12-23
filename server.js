84b2767274f88a5b81370e1d62673da79c2ccfef
Matej Bransky <matej.bransky@gmail.com>
2018-12-23 16:16:58 +0100

    complete the challenge

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-'use strict';
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-var express = require('express');
-var cors = require('cors');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-// require and use "multer"...
-
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.get('/hello', function(req, res) {
+  res.json({ greetings: 'Hello, API' });
+});
+
+app.post('/api/fileanalyse', upload.any(), (req, res) => {
+  const [file] = req.files;
+  res.json(Object.assign({}, file, { buffer: undefined }));
+});
+
+app.listen(process.env.PORT || 3000, function() {
   console.log('Node.js listening ...');
 });
